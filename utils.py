c45b94bfd8abe6377be39eb0b45cadc9272b0a96
alex210501 <alejandrobrb210501@gmail.com>
2021-02-09 14:50:38 +0100

    Fix utiles to pass test

--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
+from math import sqrt
+import scipy.integrate
+
+
 def fact(n):
 	"""Computes the factorial of a natural number.
 	
@@ -10,7 +14,10 @@ def fact(n):
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+	if n == 0:
+		return 1
+	
+	return fact(n-1) * n
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -19,7 +26,19 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+
+	# Calcul the delta
+	d_delta = b**2 - 4*a*c
+
+	#Return in function of the delta
+	if d_delta < 0:
+		return ()
+	elif d_delta == 0:
+		return ((-b) / (2 * a))
+	else:
+		x_1 = ((-b + sqrt(d_delta)) /(2 * a))
+		x_2 = ((-b - sqrt(d_delta)) / (2 * a))
+		return (x_1, x_2)
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -34,7 +53,9 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	func = lambda x:eval(function)
+
+	return scipy.integrate.quad(func, lower, upper)[0]
 
 if __name__ == '__main__':
 	print(fact(5))
